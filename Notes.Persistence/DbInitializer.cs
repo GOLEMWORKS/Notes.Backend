c8801f9380540d5e093352e4af03db8dd3beb00a
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2023-01-11 19:27:45 +0200

    Подготовка к работе с БД

---
+++ Notes.Persistence/DbInitializer.cs
@@ -0,0 +1,11 @@
+﻿namespace Notes.Persistence
+{
+    public class DbInitializer
+    {
+        public static void Initialize(NotesDbContext context)
+        {
+            context.Database.EnsureCreated();
+            //При отсутствии БД создаёт её на основе нашего представления
+        }
+    }
+}
--- Notes.Persistence/EntityTypeConfigurations/NoteConfiguration.cs
+++ Notes.Persistence/EntityTypeConfigurations/NoteConfiguration.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Notes.Domain;
 
 namespace Notes.Persistence.EntityTypeConfigurations
 {
-    internal class NoteConfiguration
+    internal class NoteConfiguration : IEntityTypeConfiguration<Note>
     {
+        public void Configure(EntityTypeBuilder<Note> builder) //Подробнее о конфигурации можно узнать в док-ции EntityFramework
+        {
+            builder.HasKey(note => note.Id);
+            builder.HasIndex(note => note.Id).IsUnique();
+            builder.Property(note => note.Title).HasMaxLength(250);
+        }
     }
 }
