c22c9b5226aa8bb3679148a8b916f09830438b30
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2023-01-12 21:34:40 +0200

    fast save

---
+++ Notes.Application/Common/Mappings/AssemblyMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using System.Reflection;
+
+namespace Notes.Application.Common.Mappings
+{
+    public class AssemblyMappingProfile : Profile
+    {
+        public AssemblyMappingProfile(Assembly assembly) =>
+            ApplyMappingsFromAssembly(assembly);
+
+        private void ApplyMappingsFromAssembly(Assembly assembly)
+        {
+
+        }
+    }
+}
