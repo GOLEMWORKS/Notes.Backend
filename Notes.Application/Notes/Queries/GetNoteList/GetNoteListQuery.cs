b99bdbfeaac9df80707219d661af35eb71022e4a
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2023-01-23 14:31:25 +0200

    mapping fin

--- Notes.Application/Notes/Queries/GetNoteList/GetNoteListQuery.cs
+++ Notes.Application/Notes/Queries/GetNoteList/GetNoteListQuery.cs
@@ -3,7 +3,7 @@ using MediatR;
 
 namespace Notes.Application.Notes.Queries.GetNoteList
 {
-    internal class GetNoteListQuery : IRequest<NoteListVm>
+    public class GetNoteListQuery : IRequest<NoteListVm>
     {
         public Guid UserId { get; set; }
     }
