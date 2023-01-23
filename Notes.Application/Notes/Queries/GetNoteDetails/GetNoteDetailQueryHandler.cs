61bb53f10a2bbcb40d09583c4e4a49e5f1bf2abc
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2023-01-23 11:09:58 +0200

    получение деталей о заметках END

---
+++ Notes.Application/Notes/Queries/GetNoteDetails/GetNoteDetailQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using MediatR;
+using Notes.Application.Interfaces;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Notes.Application.Common.Exceptions;
+using Notes.Domain;
+
+namespace Notes.Application.Notes.Queries.GetNoteDetails
+{
+    public class GetNoteDetailQueryHandler
+        : IRequestHandler<GetNoteDetailsQuery, NoteDetailsVm>
+    {
+        private readonly INotesDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public GetNoteDetailQueryHandler(INotesDbContext dbContext,
+            IMapper mapper) => (_dbContext, _mapper) = (dbContext, mapper);
+
+        public async Task<NoteDetailsVm> Handle(GetNoteDetailsQuery request, 
+            CancellationToken cancellationToken)
+        {
+            var entity = await _dbContext.Notes
+                .FirstOrDefaultAsync(note =>
+                note.Id == request.Id, cancellationToken);
+
+            if (entity == null || entity.UserId != request.UserId)
+            {
+                throw new NotFoundException(nameof(Note), request.Id);
+            }
+
+            return _mapper.Map<NoteDetailsVm>(entity);
+        }
+    }
+}
