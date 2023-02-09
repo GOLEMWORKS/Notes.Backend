ca1867925771de1b9f08ceb371f6bff9a337d337
K Vladimir <korotkih.vladimir.isr21.1@gmail.com>
2023-02-09 12:25:56 +0200

    маленький фикс

--- Notes.Application/DependencyInjection.cs
+++ Notes.Application/DependencyInjection.cs
@@ -10,7 +10,7 @@ namespace Notes.Application
         public static IServiceCollection AddApplication(
             this IServiceCollection services)
         {
-            services.AddMediatR(Assembly.GetExecutingAssembly()));
+            services.AddMediatR(Assembly.GetExecutingAssembly());
             return services;
         }
     }
